7dfdc74303d931658b7fd9e94bbd0a2a50e13619
James Pike <jpike@chilon.net>
2013-03-27 08:00:01 +0000

    Fix prelude.

--- prelude.js
+++ prelude.js
@@ -1,16 +1,12 @@
-
 if (! require && ! define) {
-  if (! window) {
+  if (! window)
     var window = {}
-  }
+
   var exports = window.domos = {},
       require = function (name) {
-        return require.modules[name.replace(/^\.\//,"")]
+        return exports
       },
-      define = function (name, ignore, module) {
-        require.modules[name] = module
+      define = function (name, deps, module) {
         module(require, exports)
       }
-
-  require.modules = {}
 }
